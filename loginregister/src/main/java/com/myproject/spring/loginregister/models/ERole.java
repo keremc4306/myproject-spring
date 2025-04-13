9bd0545aa4eb324c83fdef4851eecdefeebfec6e
Kerem Candangil <keremc4306@gmail.com>
2025-04-13 21:51:46 +0300

    13-04-2025 category of models were created.

---
+++ loginregister/src/main/java/com/myproject/spring/loginregister/models/ERole.java
@@ -0,0 +1,7 @@
+package com.myproject.spring.loginregister.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ loginregister/src/test/java/com/myproject/spring/loginregister/LoginregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.spring.loginregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
